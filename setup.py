d4c4d49855518cb054dc50ac0709216f5cd88e9b
TheAspectDev <yasharprotpc@yahoo.com>
2024-01-19 20:54:16 +0330

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UDWP',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        "websockets"
+    ],
+)
