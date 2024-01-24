964225e8fc1c51d4e7e02981874a915a7c9056eb
TheAspectDev <yasharprotpc@yahoo.com>
2024-01-24 13:44:54 +0330

    type/__inner__.py

--- UDWP/mtypes/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import Channel
-from . import User
-from . import Guild
-from . import Message
-from . import Generator
-
-__all__ = ['Channel', 'User', 'Guild', 'Message']
---
+++ UDWP/type/__init__.py
@@ -0,0 +1,7 @@
+from .Channel import Channel
+from .User import User
+from .Guild import Guild
+from .Message import Message
+from . import Generator
+
+__all__ = ['Channel', 'User', 'Guild', 'Message']
